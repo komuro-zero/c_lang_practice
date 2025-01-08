ef9749450b3c23349e66903219da32becb0a48ca
komuro-zero <komuro@zerofield.biz>
2025-01-08 23:47:00 +0900

    added print of puzzle

--- puzzle.c
+++ puzzle.c
@@ -28,3 +28,14 @@ int ** createPuzzle(){
 	
 	return array;
 }
+
+void printPuzzle(int** puzzle){
+	int i,j;
+	for (i=0; i< 9; i++){
+		for (j=0; j< 9;j++){
+
+			printf("%d", puzzle[i][j]);
+		}
+		printf("\n");
+	}
+}
